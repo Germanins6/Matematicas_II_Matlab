0a0b45f1c61bca674807cd5e26e82f93d3936072
Germán Insua <1996GInsua@gmail.com>
2020-01-15 15:50:35 +0100

    Updated MockExam

--- MockExam.m
+++ MockExam.m
@@ -1,16 +1,49 @@
+%% Excercise 1
+
+
+
+
+
 %% Excercise 2
 
+R40 = [0.8830 -0.4545 0.1170; 0.4545 0.7660 -0.4545; 0.1170 0.4545 0.8830];
+R35 = [0.9096 -0.4056 0.0904; 0.4056 0.8192 -0.4056; 0.0904 0.4056 0.9096];
 
+R5 = R40*R35';
+R10 = R5*R5;
+R50 = R10*R40
 
 %% Excercise 3 A
 R = 1/3*[2 -1 2; 2 2 -1; -1 2 2]
 
-angle = asind(-R(3,1))
+%1st Set of Angles
+pitch = asind(-R(3,1))
+roll = atan2d(R(3,2)/cosd(pitch),R(3,3)/cosd(pitch))
+yaw = atan2d(R(2,1)/cosd(pitch), R(1,1)/cosd(pitch))
+
+%2nd Set of Angles
+pitch = 180-asind(-R(3,1))
+roll = atan2d(R(3,2)/cosd(pitch),R(3,3)/cosd(pitch))
+yaw = atan2d(R(2,1)/cosd(pitch), R(1,1)/cosd(pitch))
+
 
 %% Excercise 3 B
 R = 1/3*[2 -1 2; 2 2 -1; -1 2 2]
 
-angle = acosd((trace(R)-1)/2)
+%Now working in radians not degrees!
+angle = acos((trace(R)-1)/2)
+Ux = (R-R')/2*sin(angle)
+u = [Ux(3,2) Ux(1,3) Ux(2,1)]
+
+%Now we must normalize axis
+u = u/norm(u)
+norm(u) %If norm equals 1 its done
+
+%With angle and axis norm we obtain rotation vector
+r = angle*u
+
+CheckAxis = r/norm(r)
+CheckAngle = norm(r)
 
 %% Excercise 4
 
